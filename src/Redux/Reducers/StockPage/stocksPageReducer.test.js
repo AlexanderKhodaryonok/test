86943b61824c9de4851a1713ee14bb640b91d65c
Your name <alexanderkhodaryonok@gmail.com>
2019-05-10 19:28:04 +0300

    Added describe section, test to return initial state

--- src/Redux/Reducers/StockPage/stocksPageReducer.test.js
+++ src/Redux/Reducers/StockPage/stocksPageReducer.test.js
@@ -2,51 +2,67 @@ import stocksReducer from "./stocksPageReducer";
 import { setStockId, setShowMode, setFilter, moveStock, setStatus, setStocks } from "../../actionCreators/stockPageActionCreators";
 import { statuses } from '../../../helpers/statuses/statuses';
 
-it('id is set correctly', () => {
-  let newState = stocksReducer({
-    currentStockId: '',
-  }, setStockId('3'));
-  expect(newState.currentStockId).toEqual('3');
-});
+describe('stocksPageReducers', () => {
 
-it('showMode is set correctly', () => {
-  let newState = stocksReducer({
-    isShowMyStocks: false,
-  }, setShowMode(true));
-  expect(newState.isShowMyStocks).toEqual(true);
-});
+  it('should return the initial state', () => {
+    expect(stocksReducer(undefined, {})).toEqual(
+      {
+        status: statuses.init,
+        filter: '',
+        isShowMyStocks: false,
+        currentStockId: '',
+        stocks: [],
+      }
+    )
+  })
 
-it('setFilter is set correctly', () => {
-  let newState = stocksReducer({
-    filter: '',
-  }, setFilter('stock'));
-  expect(newState.filter).toEqual('stock');
-});
+  it('id is set correctly', () => {
+    let newState = stocksReducer({
+      currentStockId: '',
+    }, setStockId('3'));
+    expect(newState.currentStockId).toEqual('3');
+  });
 
-it('stock is added/removed correctly', () => {
-  let newState = stocksReducer({
-    stocks: [{
-      id: '3',
-      isAdded: false,
-    },]
-  }, moveStock({ bool: true, id: '3' }));
-  expect(newState.stocks[0].isAdded).toEqual(true);
-});
+  it('showMode is set correctly', () => {
+    let newState = stocksReducer({
+      isShowMyStocks: false,
+    }, setShowMode(true));
+    expect(newState.isShowMyStocks).toEqual(true);
+  });
 
-it('status is set correctly', () => {
-  let newState = stocksReducer({
-    status: statuses.init,
-  }, setStatus(statuses.inProgress));
-  expect(newState.status).toEqual(statuses.inProgress);
-});
+  it('setFilter is set correctly', () => {
+    let newState = stocksReducer({
+      filter: '',
+    }, setFilter('stock'));
+    expect(newState.filter).toEqual('stock');
+  });
 
-it('stocks is set correctly', () => {
-  let newState = stocksReducer({
-    stocks: []
-  }, setStocks(
-    { stocks: [{ a: 'qwe' }] }
-  ));
-  expect(newState.stocks).toEqual([{
-    a: 'qwe'
-  }]);
+  it('stock is added/removed correctly', () => {
+    let newState = stocksReducer({
+      stocks: [{
+        id: '3',
+        isAdded: false,
+      },]
+    }, moveStock({ bool: true, id: '3' }));
+    expect(newState.stocks[0].isAdded).toEqual(true);
+  });
+
+  it('status is set correctly', () => {
+    let newState = stocksReducer({
+      status: statuses.init,
+    }, setStatus(statuses.inProgress));
+    expect(newState.status).toEqual(statuses.inProgress);
+  });
+
+  it('stocks is set correctly', () => {
+    let newState = stocksReducer({
+      stocks: []
+    }, setStocks(
+      { stocks: [{ id: '1' }] }
+    ));
+    expect(newState.stocks).toEqual([{
+      id: '1'
+    }]);
+  });
+
 });
